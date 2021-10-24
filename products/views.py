ae3adaf315730fdb63dd28c4a5f34ec58907c695
Christian <christian_nagata@hotmail.com>
2021-10-24 19:30:03 -0300

    Criação da página do produto

--- products/views.py
+++ products/views.py
@@ -6,3 +6,9 @@ def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'products/index.html', context)
+
+
+def product(request, product_id):
+    product_info = Product.objects.get(id=product_id)
+    context = {'info': product_info}
+    return render(request, 'products/product.html', context)
